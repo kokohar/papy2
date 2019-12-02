98b3bbd984aca7ec379259323ee5f73cc9bf1ccc
kokohar <39835860+kokohar@users.noreply.github.com>
2019-12-02 08:27:36 +0100

    Add files via upload

---
+++ script-extensions/deploy-ad1.ps1
@@ -0,0 +1,13 @@
+﻿param($domain, $password)
+
+$smPassword = (ConvertTo-SecureString $password -AsPlainText -Force)
+
+Install-WindowsFeature -Name "AD-Domain-Services" `
+                       -IncludeManagementTools `
+                       -IncludeAllSubFeature 
+
+ Install-ADDSDomainController -DomainName $domain `
+                   -DomainMode Win2012 `
+                   -ForestMode Win2012 `
+                   -Force `
+                   -SafeModeAdministratorPassword $smPassword 
